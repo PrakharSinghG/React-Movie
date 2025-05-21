cb5a303d23d9a16b78a7618af305eef582758dee
Prakhar Singh <prakharsingh2001@gmail.com>
2025-05-21 16:04:47 +0530

    Hiding Api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_KEY = "81a3a015260553100666d85edfc15522";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
